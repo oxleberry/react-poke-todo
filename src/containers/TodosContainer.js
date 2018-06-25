99a23ee2e13dc796562dea507da7570a3411f515
oxleberry <oxleberry@gmail.com>
2018-06-25 12:11:56 -0700

    setup links and routes

---
+++ src/containers/TodosContainer.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react';
+
+class TodosContainer extends Component {
+    render () {
+        return (
+            <div className='todosContainer'>
+                <h2>This container will hold all of my Pokemon</h2>
+            </div>
+        )
+    }
+}
+
+export default TodosContainer;
