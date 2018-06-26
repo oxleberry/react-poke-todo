a81e4c7e9a9567b66f5473b62a3fb90a3823320f
oxleberry <oxleberry@gmail.com>
2018-06-26 10:49:15 -0700

    added Create feature

--- src/models/Todo.js
+++ src/models/Todo.js
@@ -3,9 +3,14 @@ import axios from 'axios';
 
 class TodoModel {
    static all(){
-      let request = axios.get("http://super-crud-api.herokuapp.com/api/pokemon")
-      return request
+     let request = axios.get("http://super-crud-api.herokuapp.com/api/pokemon")
+     return request
    }
+   static create(todo) {
+      console.log(todo);
+    let request = axios.post("http://super-crud-api.herokuapp.com/api/pokemon", todo)
+    return request
+  }
 }
 
 export default TodoModel
